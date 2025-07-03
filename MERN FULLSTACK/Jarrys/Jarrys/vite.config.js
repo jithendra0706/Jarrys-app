1364e4e1d34df50a5501960e9eeb40812417169f
jithendra0706 <dasari.jithendra.d@gmail.com>
2025-07-03 10:18:39 +0530

    Update vite.config.js

--- MERN FULLSTACK/Jarrys/Jarrys/vite.config.js
+++ MERN FULLSTACK/Jarrys/Jarrys/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Jarrys-app/", // 👈 IMPORTANT: this is your repo name
   plugins: [react()],
 })
